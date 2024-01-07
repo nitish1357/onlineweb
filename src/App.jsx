82060ac8b41dde445ea51d98bdfe32dffa86d7b1
Nitish1357 <nitishmehta1990@gmail.com>
2024-01-07 14:39:11 +0530

    setup index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import HomeLayout from './pages/HomeLayout';
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from './pages';
 
 const router = createBrowserRouter([
   {
@@ -7,12 +14,16 @@ const router = createBrowserRouter([
     element: <HomeLayout />,
   },
   {
-    path: '/about',
-    element: (
-      <div>
-        <h1>About page</h1>
-      </div>
-    ),
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout />,
   },
 ]);
 
