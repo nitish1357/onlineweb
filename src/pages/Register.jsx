b80d5b9eaa4894026ed4b574c228b1183ac8c477
Nitish1357 <nitishmehta1990@gmail.com>
2024-01-07 14:45:16 +0530

    Link component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-  return <h1>Register Page</h1>;
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <Link to="/login">Login page</Link>
+    </div>
+  );
 };
 export default Register;
