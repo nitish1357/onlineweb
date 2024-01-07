2db76e3e76325009cea301ac1f598cc533624665
Nitish1357 <nitishmehta1990@gmail.com>
2024-01-07 15:17:10 +0530

    styled components intro

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,17 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  font-size: 1.5rem;
+  background-color: red;
+  color: white;
+`;
+
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div>
+      <h1>Landing</h1>
+      <StyledButton>Click Me</StyledButton>
+    </div>
+  );
 };
 export default Landing;
